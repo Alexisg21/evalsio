8aa1db2e0b9fe13399016ad755fa45d00c9f8dbe
E20253877 <gaillardalexis21@gmail.com>
2025-10-06 09:21:06 +0200

    maj

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const input= document.getElementById("nom");
+const zone= document.getElementById("message");
+const bouton= document.getElementById("valider");
+bouton.addEventListener("click", function () {
+    zone.innerHTML="Bonjour "+input.value+"";
+    console.log("Bonjour "+input.value);
+    if(input.value==""){
+        zone.innerHTML="Veuillez entrer votre nom";
+        zone.style.color = "blue";
+        zone.style.fontSize = "40px";
+        zone.style.center = "center";
+    }
+});
